8657bfac0a5a111c27028a3ca68a2d963c5dacee
Leyder Leal <leyder.leal2005@gmail.com>
2024-02-07 21:34:59 -0500

    Update settings.py

--- GestionInventario/settings.py
+++ GestionInventario/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = 'django-insecure-z@=q(5@hjf5@+h3u-h5v2dyio4uh+zi%99lv0f0g*5g8y4g20%
 #variables configuracion correo
 EMAIL_HOST= "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER= "leyder.leal2005@gmail.com"
-EMAIL_HOST_PASSWORD= "kgar yuwu cxbx adnq"
+EMAIL_HOST_USER= ""
+EMAIL_HOST_PASSWORD= ""
 EMAIL_USE_TLS= True
 
 # SECURITY WARNING: don't run with debug turned on in production!
